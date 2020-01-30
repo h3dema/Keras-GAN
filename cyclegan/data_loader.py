139d815a24b43c013fac32d017ddec8cd1eb6134
h3dema <henriquemoura@hotmail.com>
2020-01-30 12:47:32 +0100

    updates

--- cyclegan/data_loader.py
+++ cyclegan/data_loader.py
@@ -2,7 +2,6 @@ import scipy
 from glob import glob
 import numpy as np
 
-
 class DataLoader():
     def __init__(self, dataset_name, img_res=(128, 128)):
         self.dataset_name = dataset_name
@@ -18,14 +17,14 @@ class DataLoader():
         for img_path in batch_images:
             img = self.imread(img_path)
             if not is_testing:
-                img = self.imresize(img, self.img_res)
+                img = scipy.misc.imresize(img, self.img_res)
 
                 if np.random.random() > 0.5:
                     img = np.fliplr(img)
             else:
-                img = self.imresize(img, self.img_res)
+                img = scipy.misc.imresize(img, self.img_res)
             imgs.append(img)
-        imgs = np.array(imgs) / 127.5 - 1.
+        imgs = np.array(imgs)/127.5 - 1.
 
         return imgs
 
@@ -37,21 +36,21 @@ class DataLoader():
         self.n_batches = int(min(len(path_A), len(path_B)) / batch_size)
         total_samples = self.n_batches * batch_size
 
-        # Sample n_batches * batch_size from each path list so that model
-        # sees all samples from both domains
+        # Sample n_batches * batch_size from each path list so that model sees all
+        # samples from both domains
         path_A = np.random.choice(path_A, total_samples, replace=False)
         path_B = np.random.choice(path_B, total_samples, replace=False)
 
-        for i in range(self.n_batches - 1):
-            batch_A = path_A[i * batch_size:(i + 1) * batch_size]
-            batch_B = path_B[i * batch_size:(i + 1) * batch_size]
+        for i in range(self.n_batches-1):
+            batch_A = path_A[i*batch_size:(i+1)*batch_size]
+            batch_B = path_B[i*batch_size:(i+1)*batch_size]
             imgs_A, imgs_B = [], []
             for img_A, img_B in zip(batch_A, batch_B):
                 img_A = self.imread(img_A)
                 img_B = self.imread(img_B)
 
-                img_A = self.imresize(img_A, self.img_res)
-                img_B = self.imresize(img_B, self.img_res)
+                img_A = scipy.misc.imresize(img_A, self.img_res)
+                img_B = scipy.misc.imresize(img_B, self.img_res)
 
                 if not is_testing and np.random.random() > 0.5:
                         img_A = np.fliplr(img_A)
@@ -60,33 +59,16 @@ class DataLoader():
                 imgs_A.append(img_A)
                 imgs_B.append(img_B)
 
-            imgs_A = np.array(imgs_A) / 127.5 - 1.
-            imgs_B = np.array(imgs_B) / 127.5 - 1.
+            imgs_A = np.array(imgs_A)/127.5 - 1.
+            imgs_B = np.array(imgs_B)/127.5 - 1.
 
             yield imgs_A, imgs_B
 
-    def imresize(self, img, size):
-        """workaroung imresize"""
-        from PIL import Image
-        im = Image.fromarray(img)
-        size = tuple((np.array(im.size) * 0.99999).astype(int))
-        new_image = np.array(im.resize(size, Image.BICUBIC))
-        return new_image
-
     def load_img(self, path):
         img = self.imread(path)
-
-        # scipy.misc.imresize is deprecated!
-        # imresize is deprecated in SciPy 1.0.0, and will be removed in 1.3.0
-        # img = scipy.misc.imresize(img, self.img_res)
-        img = self.imresize(img, self.img_res)
-        img = img / 127.5 - 1.
+        img = scipy.misc.imresize(img, self.img_res)
+        img = img/127.5 - 1.
         return img[np.newaxis, :, :, :]
 
     def imread(self, path):
-        # imread is deprecated!
-        # imread is deprecated in SciPy 1.0.0, and will be removed in 1.2.0.
-        # return scipy.misc.imread(path, mode='RGB').astype(np.float)
-
-        import imageio
-        return imageio.imread(path, pilmode='RGB').astype(np.int)
+        return scipy.misc.imread(path, mode='RGB').astype(np.float)
